e485227ac2d6c189f07f4460c3a4e666650df4dc
ConnorStine <49596244+ConnorStine@users.noreply.github.com>
2021-04-21 19:23:31 -0500

    Added goodWorker.py functionality
    driver.py now implements 5 workers, 2 bad, 3 good. Small changes to comments and output.

--- driver.py
+++ driver.py
@@ -24,10 +24,17 @@ workers.append(badWorker2)
 goodWorker1 = GOOD.goodWorker()
 goodWorker1.setID(3)
 workers.append(goodWorker1)
+goodWorker2 = GOOD.goodWorker()
+goodWorker2.setID(4)
+workers.append(goodWorker2)
+goodWorker3 = GOOD.goodWorker()
+goodWorker3.setID(5)
+workers.append(goodWorker3)
+
 # Loop until exit
 while looping:
 	userIn = input("Please enter your chosen input: ")
-	if userIn == "exit":	# Exit signal received
+	if userIn == "exit": # Exit signal received
 		print("Exit signal received, goodbye!")
 		time.sleep(1)
 		looping = False
@@ -35,39 +42,38 @@ while looping:
 		print("\nCURRENT STATUS OF ALL WORKERS:\n")
 		for w in workers:
 			print("Worker {}:".format(w.getID()))
-			if w.getID() is 3:
-                        	print("GOOD WORKER!")
+			if w.getID() > 2:
+				print("< GOOD WORKER >")
 			else:
-				print("BAD WORKER!")
+				print("< BAD WORKER >")
 			print("    Total Tasks Claimed:    ", w.getTotalTasks())
 			print("    Total Successful Tasks: ", w.getCorrectTasks())
 			print("    Total Currency Gained:  ", w.getCurrency())
-			print("")	# skip a line
-	else:		# This should be a/an int List(s)
-		if userIn == "": 	# Clarity spacing if people want it
+			print("") # skip a line
+	else: # This should be a/an int List(s)
+		if userIn == "": # Clarity spacing if people want it
 			continue
 		try:
-			sets = userIn.split(',')	# Split input up by commas just in case
+			sets = userIn.split(',') # Split input up by commas just in case
 			for item in sets:
 				set = item.split()
-				set = list(map(int, set))	# Make sure all items are numbers in the List
+				set = list(map(int, set)) # Make sure all items are numbers in the List
 				oSet = []
-				for number in set:		# copy original set
+				for number in set: # Copy original set
 					oSet.append(number)
-				#print(random.randint(1,5))	# Choose a random worker
 				
 				# Begin handling all sets
 				sorting = True
 				while sorting:
 					print("\nPutting set", oSet, "up for sorting.")
-					time.sleep(random.uniform(0.1, 0.8))	# random sleep to facilitate visibility and randomness
-					rand = random.randint(0,2)	# pick a worker
+					time.sleep(random.uniform(0.1, 0.8)) # Random sleep to facilitate visibility and randomness
+					rand = random.randint(0,4) # Pick a worker
 					print("Task taken by worker", rand + 1)
 					workers[rand].addTask(set)
 					rSet = workers[rand].chanceSort()
 					time.sleep(0.5)	# "task time"
 					print("Set sorted by Worker {}: {}".format(rand + 1, rSet))
-					# if sorted, continue on, if not, put it up again
+					# If sorted, continue on, if not, put it up again
 					if all(rSet[i] <= rSet[i+1] for i in range(len(rSet)-1)):
 						print("Set correctly sorted! Currency paid.\n_\n")
 						workers[rand].setCurrency(workers[rand].getCurrency() + len(rSet))
